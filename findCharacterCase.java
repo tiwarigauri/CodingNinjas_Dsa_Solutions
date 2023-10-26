a4c6523319b466e7f9d45eb21ed9816fc1a4eab6
tiwarigauri <gauri.uiet@gmail.com>
2023-10-27 00:01:38 +0530

    solution of find character case

---
+++ findCharacterCase.java
@@ -0,0 +1,17 @@
+package Coding_Ninjas_Dsa_Solutions;
+
+import java.util.*;
+
+public class findCharacterCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        if (Character.isUpperCase(c)) {
+            System.out.println("1");
+        } else if (Character.isLowerCase(c)) {
+            System.out.println("0");
+        } else {
+            System.out.println("-1");
+        }
+    }
+}
